71e2eed535dcfb7b28c187bbe2cf73719b49fdcd
Lukas <projekt-info2013@bwk-cpg.de>
2014-01-21 20:31:34 +0100

    Grundstruktur des Projekts

---
+++ SignalGame/src/de/cpg_gilching/informatik12/signalgame/ClientLauncher.java
@@ -0,0 +1,9 @@
+package de.cpg_gilching.informatik12.signalgame;
+
+public class ClientLauncher {
+	
+	public static void main(String[] args) {
+		// TODO
+	}
+	
+}
---
+++ SignalGame/src/de/cpg_gilching/informatik12/signalgame/ServerLauncher.java
@@ -0,0 +1,9 @@
+package de.cpg_gilching.informatik12.signalgame;
+
+public class ServerLauncher {
+	
+	public static void main(String[] args) {
+		// TODO
+	}
+	
+}
